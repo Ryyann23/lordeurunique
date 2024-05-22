9422a118540213970082530138dc235936bfb83e
LuanCoutinhoo <luansalles698@gmail.com>
2024-05-22 15:35:52 -0300

    menumobile

---
+++ menumobile.js
@@ -0,0 +1,9 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event){
+    if(event.type === 'touchstart') event.preventDefaul
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+btnMobile.addEventListener('click',toggleMenu);
+btnMobile.addEventListener('touchstart',toggleMenu);
